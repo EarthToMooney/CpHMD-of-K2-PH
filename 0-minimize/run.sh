966edda79a00fc788b3c0d040a7f4953a49df8b0
Eric Mooney <77983993+EarthToMooney@users.noreply.github.com>
2021-11-22 11:59:23 -0500

    Add files via upload

---
+++ 0-minimize/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --clusters=amarel            # Select which system(s) to use
+#SBATCH --partition=main             # Partition (job queue)
+#SBATCH --requeue                    # Return job to the queue if preempted
+#SBATCH --job-name=<name>             # Assign an short name to your job
+#SBATCH --nodes=1                    # Number of nodes you require
+#SBATCH --ntasks=32                 # Total # of tasks across all nodes
+#SBATCH --cpus-per-task=1            # Cores per task (>1 if multithread tasks)
+#SBATCH --mem=16G                     # Real memory (RAM) required per node
+#SBATCH --time=01:00:00                 # Total run time limit (DD-HH:MM:SS)
+#SBATCH --output=slurm.%N.%j.out     # STDOUT file for SLURM output
+
+cd $PWD
+
+## Environment settings needed for this job
+module load intel/17.0.1  mvapich2/2.2  cuda/7.5
+module load amber/16
+
+## Run the job
+sander.MPI -O -i min.mdin -p 2lko.parm7 -c 2lko.rst7 -o 0_min.out -r 0_min.rst7 -x 0_min.nc -ref 2lko.rst7 -cpin 2lko.cpin
+
+
