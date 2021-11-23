06ab4ec94aba8ea4335bfb38ef32832f4c05d809
Eric Mooney <77983993+EarthToMooney@users.noreply.github.com>
2021-11-23 00:44:27 -0500

    Add files via upload

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 #SBATCH --clusters=amarel            # Select which system(s) to use
 #SBATCH --partition=main             # Partition (job queue)
 #SBATCH --requeue                    # Return job to the queue if preempted
-#SBATCH --job-name=<name>             # Assign an short name to your job
+#SBATCH --job-name=3PH8             # Assign an short name to your job
 #SBATCH --nodes=1                    # Number of nodes you require
-#SBATCH --ntasks=32                 # Total # of tasks across all nodes
+#SBATCH --ntasks=30                 # Total # of tasks across all nodes
 #SBATCH --cpus-per-task=1            # Cores per task (>1 if multithread tasks)
-#SBATCH --mem=16G                     # Real memory (RAM) required per node
+#SBATCH --mem-per-cpu=3000          # mem requested per CPU in MB
 #SBATCH --time=72:00:00                 # Total run time limit (DD-HH:MM:SS)
 #SBATCH --output=slurm.%N.%j.out     # STDOUT file for SLURM output
 
@@ -16,6 +16,5 @@ cd $PWD
 module load intel/17.0.1  mvapich2/2.2  cuda/7.5
 module load amber/16
 
-## Run the job
-sander.MPI -O -i heat.mdin -p 2lko.parm7 -c a_min.rst7 -o 1_heat.out -r 1_heat.rst7 -x 1_heat.nc -ref a_min.rst7 -cpin 2lko.cpin
+srun --mpi=pmi2 sander.MPI -O -i md.mdin -p 2lko.parm7 -c 2lko.equil.rst7 -cpin 2lko.equil.cpin -o 2lko.production_pH8.mdout -cpout 2lko.production_pH8.cpout -r 2lko.production_pH8.rst7 -x 2lko.production_pH8.nc -cprestrt 2lko.production_pH8.cpin ## Run the job
 
