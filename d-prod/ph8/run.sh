4eb01cf3f72216f2824aa4f587425c77b4c3c55a
Eric Mooney <77983993+EarthToMooney@users.noreply.github.com>
2021-11-23 00:53:35 -0500

    Add files via upload

---
+++ d-prod/ph8/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --clusters=amarel            # Select which system(s) to use
+#SBATCH --partition=main             # Partition (job queue)
+#SBATCH --requeue                    # Return job to the queue if preempted
+#SBATCH --job-name=dPH8             # Assign an short name to your job
+#SBATCH --nodes=2                    # Number of nodes you require
+#SBATCH --ntasks=38                 # Total # of tasks across all nodes
+#SBATCH --cpus-per-task=1            # Cores per task (>1 if multithread tasks)
+#SBATCH --mem-per-cpu=3000          # mem requested per CPU in MB
+#SBATCH --time=72:00:00                 # Total run time limit (DD-HH:MM:SS)
+#SBATCH --output=slurm.%N.%j.out     # STDOUT file for SLURM output
+
+cd $PWD
+
+## Environment settings needed for this job
+module load intel/17.0.1  mvapich2/2.2  cuda/7.5
+module load amber/16
+
+srun --mpi=pmi2 sander.MPI -O -i md.mdin -p 2lko.parm7 -c 2lko.equil.rst7 -cpin 2lko.equil.cpin -o 2lko.production_pH8.mdout -cpout 2lko.production_pH8.cpout -r 2lko.production_pH8.rst7 -x 2lko.production_pH8.nc -cprestrt 2lko.production_pH8.cpin ## Run the job
